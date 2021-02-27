78b5333943b218977fbd90f5b3dd0ae7bada80dc
Pavel <pl.hancharou@gmail.com>
2021-02-27 22:34:15 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return Array.isArray(array) && array.length ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return Array.isArray(array) && array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if ((Array.isArray(array) && array.length)) {
+    const result = array.reduce((acc, item) => acc += item);
+    return result / array.length;
+  } else {
+    return 0;
+  }
 }
